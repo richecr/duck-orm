88b4f9601743f667e692d78a8a8fc8055b3129a1
Rich <richelton14@gmail.com>
2021-06-21 23:25:15 -0300

    Correction tests relationships

--- tests/test_duck_postgres_relationship.py
+++ tests/test_duck_postgres_relationship.py
@@ -3,7 +3,7 @@ import functools
 import asyncio
 import pytest
 
-from duck_orm.Model import Model
+from duck_orm.model import Model
 from duck_orm.sql import fields as Field
 from duck_orm.sql.relationship import ManyToOne, OneToOne, OneToMany
 
@@ -15,21 +15,25 @@ class City(Model):
     __db__ = db
 
     id: int = Field.Integer(
-        primary_key=True, auto_increment=True)
+        primary_key=True,
+        auto_increment=True)
     name: str = Field.String(unique=True)
 
     @classmethod
     def relationships(cls):
         cls.persons = OneToMany(
-            model=Person, name_in_table_fk='city', name_relation='person_city')
+            model=Person,
+            name_in_table_fk='city',
+            name_relation='person_city')
 
 
 class Person(Model):
     __tablename__ = 'persons'
     __db__ = db
 
-    id: int = Field.Integer(
-        primary_key=True, auto_increment=True)
+    id_teste: int = Field.Integer(
+        primary_key=True,
+        auto_increment=True)
     first_name: str = Field.String(unique=True)
     last_name: str = Field.String(not_null=True)
     age: int = Field.BigInteger(min_value=18)
@@ -41,28 +45,15 @@ class Contact(Model):
     __tablename__ = 'contacts'
     __db__ = db
 
-    id_person: Person = OneToOne(model=Person)
+    id_person = Field.Integer(primary_key=True)
     phone: str = Field.String(not_null=True)
 
-
-# Creating instances of Cities.
-city_cg = City(name="Campina Grande")
-city_kh = City(name="Konoha")
-
-# Creating instances of Persons.
-person_1 = Person(first_name="Rich", last_name="Ramalho",
-                  age=22, salary=1250, city=city_cg)
-person_2 = Person(first_name="Elton", last_name="Ramalho",
-                  age=22, salary=1250)
-person_3 = Person(first_name="Naruto", last_name="Uzumaki",
-                  age=16, salary=500000)
-person_4 = Person(first_name="Hinata", last_name="Hyuga",
-                  age=16, salary=500000)
-
-# Creating instances of Contacts.
-# contact_person_1 = Contact(phone="XXXXXXXXX-XXXX", id_person=person_1)
-# contact_person_2 = Contact(phone="YYYYYYYYY-YYYY", id_person=person_2)
-# contact_error = Contact(phone="YYYYYYYYY-YYYY", id_person=person_2)
+    @classmethod
+    def relationships(cls):
+        cls.id_person: Person = OneToOne(
+            model=Person,
+            name_relation='person_contact',
+            field='id_person')
 
 
 def async_decorator(func):
@@ -80,23 +71,21 @@ def async_decorator(func):
 
 
 def test_model_class():
-    assert City._get_name() == 'cities'
-    assert Person._get_name() == 'persons'
-    # assert Contact._get_name() == 'contacts'
-    # assert isinstance(Contact.id_person, OneToOne)
-    assert isinstance(Person.city, OneToMany)
+    assert City.get_name() == 'cities'
+    assert Person.get_name() == 'persons'
+    assert Contact.get_name() == 'contacts'
+    assert isinstance(Person.city, ManyToOne)
 
 
 def test_create_sql():
-    sql = Person.__get_create_sql()
+    sql = Person._get_create_sql()
     assert sql == "CREATE TABLE IF NOT EXISTS persons (" + \
         "salary BIGINT, " + \
         "last_name TEXT NOT NULL, " + \
-        "id SERIAL PRIMARY KEY, " + \
+        "id_teste SERIAL PRIMARY KEY, " + \
         "first_name TEXT UNIQUE, " + \
         "age BIGINT, " + \
-        "city INTEGER, " + \
-        " FOREIGN KEY (city) REFERENCES cities (id));"
+        "city INTEGER);"
 
 
 def get_table(table, tables):
@@ -112,6 +101,11 @@ async def test_create_table():
     await City.create()
     await Person.create()
     await Contact.create()
+
+    await City.associations()
+    await Person.associations()
+    await Contact.associations()
+
     tables = await Person.find_all_tables()
     assert get_table('persons', tables)
     assert get_table('contacts', tables)
@@ -122,6 +116,9 @@ async def test_create_table():
 async def test_save_city():
     global city_cg
     global city_kh
+    # Creating instances of Cities.
+    city_cg = City(name="Campina Grande")
+    city_kh = City(name="Konoha")
     city_cg = await City.save(city_cg)
     city_kh = await City.save(city_kh)
     assert city_kh.name == 'Konoha'
@@ -133,12 +130,21 @@ async def test_save_person():
     global person_1
     global person_2
     global person_3
-    person_1 = await Person.save(person_1)
-    person_2 = await Person.save(person_2)
-    person_3 = await Person.save(person_3)
-    assert person_1.first_name == 'Rich'
-    assert person_2.first_name == 'Elton'
-    assert person_3.first_name == 'Naruto'
+    # Creating instances of Persons.
+    person_1 = Person(first_name="Rich", last_name="Ramalho",
+                      age=22, salary=1250)
+    person_2 = Person(first_name="Elton", last_name="Ramalho",
+                      age=22, salary=1250)
+    person_3 = Person(first_name="Naruto", last_name="Uzumaki",
+                      age=16, salary=500000)
+    person_4 = Person(first_name="Hinata", last_name="Hyuga",
+                      age=16, salary=500000)
+    person_1_: Person = await city_kh.persons.add(person_1)
+    person_2_: Person = await city_kh.persons.add(person_2)
+    person_3_: Person = await city_kh.persons.add(person_3)
+    assert person_1_.first_name == 'Rich'
+    assert person_2_.first_name == 'Elton'
+    assert person_3_.first_name == 'Naruto'
 
 
 @async_decorator
@@ -146,6 +152,10 @@ async def test_save_contact():
     global contact_person_1
     global contact_person_2
     global contact_error
+    # Creating instances of Contacts.
+    contact_person_1 = Contact(phone="XXXXXXXXX-XXXX", id_person=person_1)
+    contact_person_2 = Contact(phone="YYYYYYYYY-YYYY", id_person=person_2)
+    contact_error = Contact(phone="YYYYYYYYY-YYYY", id_person=person_2)
     contact_person_1 = await Contact.save(contact_person_1)
     contact_person_2 = await Contact.save(contact_person_2)
     assert contact_person_1.phone == 'XXXXXXXXX-XXXX'
