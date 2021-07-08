7965c145be3f38537720c884f4f8becd67b29114
Rich <richelton14@gmail.com>
2021-07-08 18:14:00 -0300

    Correction imports

--- duck_orm/sql/relationship.py
+++ duck_orm/sql/relationship.py
@@ -1,10 +1,9 @@
-from duck_orm.sql.sqlite import QuerySQLite
-from duck_orm.sql.postgres import QueryPostgres
 import inspect
 from typing import Any, Dict, Type
 
 from duck_orm.model import Model
 from duck_orm.sql.fields import Column
+from duck_orm.sql.sqlite import QuerySQLite
 from duck_orm.sql.condition import Condition
 from duck_orm.utils.functions import get_dialect
 
